47548fc0887901003e56cee21b2bcb39d105633a
LuisJair <jair.escalante@phs.mx>
2025-09-21 10:47:48 -0600

    fix:fixed background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,9 @@ import { Technologies } from "../components/Technologies";
 
 export const Home = () => {
   return (
-    <div className="bg-white dark:bg-black transition-all">
+    <div className="bg-white bg-gradient-to-bl to-10% from-purple-700 to-white dark:to-black transition-all">
       <NavBar />
       <AboutMe />
-      <Technologies />
-      <Technologies />
       <Technologies />
     </div>
   );
