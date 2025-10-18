853835644186f25657e5889f66ca94f94e7b9c4b
Luis Jair <jair.escalante@phs.mx>
2025-10-17 22:32:06 -0600

    refactor: change styles of experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,28 +1,30 @@
 import listExperience from "../mocks/experience.mock";
+import girlComputer from "../assets/backgrounds/home-girl-modified.png";
 
 export const Experience = () => {
   return (
-    <section id="experience">
-      <h1 className="text-white text-center text-6xl font-bold">Experience</h1>
+    <section id="experience" className="h-screen pt-40 border-b-2 relative overflow-hidden">
+      <img src={girlComputer} alt="experience" className="absolute bottom-0 w-1/3 drop-shadow-xl drop-shadow-gray-800" />
+      <h1 className="text-center text-6xl font-bold commic-text">Experience</h1>
       <div className="flex justify-center mt-5 md:mt-10 relative pb-5">
         <div className="">
           {listExperience.map(({ title, date, location, toDo }, index) => (
             <div
               key={index}
-              className="dark:text-white flex items-start w-full"
+              className="flex items-start w-full"
             >
               <div className="flex flex-col w-1/3 relative">
-                <span className="bg-purple-400 text-center py-0.5 font-bold rounded-full mr-3 text-white hover:scale-105 transition-all cursor-pointer text-[12px] md:text-base">
+                <span className="bg-black text-center py-0.5 font-bold rounded-full mr-3 text-white hover:scale-105 transition-all cursor-pointer text-[12px] md:text-base">
                   {date}
                 </span>
-                <span className="p-1 bg-white border-4 border-purple-500 rounded-full w-4 h-4 absolute -right-2.5 top-1"></span>
+                <span className="p-1 bg-white border-4 rounded-full w-4 h-4 absolute -right-2.5 top-1"></span>
               </div>
 
-              <div className="px-5 border-l-2 border-purple-500 w-2/3">
+              <div className="px-5 border-l-2 w-2/3">
                 <span className="text-[14px] md:text-lg font-bold">
                   {title} - {location}
                 </span>
-                <ul className="px-5 list-disc text-[10px]">
+                <ul className="px-5 list-disc text-[15px]">
                   {toDo.map((item, ind) => (
                     <li key={ind}>{item}</li>
                   ))}
