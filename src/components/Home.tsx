6e4e7098a293f84b520895236fab485c0f2c3e51
Luis Jair <jair.escalante@phs.mx>
2025-10-17 22:00:04 -0600

    feat: add button to cv and github

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,13 @@
 import "../styles/home.css";
 import profile from "../assets/image-profile.png";
+import { FaFileDownload } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+
 export const Home = () => {
   return (
-    <section id="home" className="h-screen relative border-b-2">
+    <section id="home" className="h-screen border-b-2 overflow-hidden relative">
       {/* card presentation */}
-      <div className="h-full flex flex-col items-center pt-40">
+      <div className="flex flex-col items-center pt-40">
         <div className="flex items-center gap-10 w-2/3 p-5 card-me">
           <div className="w-2/6">
             <img src={profile} alt="profile picture" className="rounded-full" />
@@ -28,6 +31,16 @@ export const Home = () => {
             </p>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center pt-40 gap-10">
+        <button className="rounded-md px-10 py-5 font-bold text-xl cursor-pointer button-download flex items-center gap-2">
+          <FaFileDownload />
+          <span>Descargar CV</span>
+        </button>
+        <button className="rounded-md px-10 py-5 font-bold text-xl cursor-pointer button-download flex items-center gap-2">
+          <IoLogoGithub />
+          <span>Repositorio</span>
+        </button>
       </div>
     </section>
   );
